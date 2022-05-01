98db0639efdcfa98f393d51a3ea4f81ae605fd14
Earl Hibbs <hibbse77737@live.bucks.edu>
2022-05-01 04:03:36 +0000

    Jacob's Main class (for CreditCardInput)

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		
+		//CreditCardInput newCard=new CreditCardInput();
+		
+		CreditCardInput creditCard=new CreditCardInput();
+		System.out.println(creditCard.toString()); 
+
+	}
+
+}
